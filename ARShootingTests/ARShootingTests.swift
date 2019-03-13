103a2b2ca990c320a7df18bfd42b69423e7744d4
TATSUHIRO <>
2019-03-13 12:04:41 +0900

    first commit

---
+++ ARShootingTests/ARShootingTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARShootingTests.swift
+//  ARShootingTests
+//
+//  Created by TATSUHIRO on 2019/03/05.
+//  Copyright © 2019年 TATSUHIRO. All rights reserved.
+//
+
+import XCTest
+@testable import ARShooting
+
+class ARShootingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
